1f98e90da969f1ac0e8b293844fbfd10bd35b209
Tiago Bello <61912931+Tiagohbello@users.noreply.github.com>
2024-03-21 00:23:43 -0300

    swagger configs (#1)

---
+++ loomi_hub/configuration/default_schema.py
@@ -0,0 +1,57 @@
+from collections import OrderedDict
+
+from drf_yasg.inspectors import SwaggerAutoSchema
+from drf_yasg.utils import no_body
+
+
+class Response:
+    def get_fields(self):
+        new_fields = OrderedDict()
+        for fieldName, field in super().get_fields().items():
+            if not field.write_only:
+                new_fields[fieldName] = field
+        return new_fields
+
+
+class Request:
+    def get_fields(self):
+        new_fields = OrderedDict()
+        for fieldName, field in super().get_fields().items():
+            if not field.read_only:
+                new_fields[fieldName] = field
+        return new_fields
+
+
+class BlankMeta:
+    pass
+
+
+class ReadWriteAutoSchema(SwaggerAutoSchema):
+    def get_view_serializer(self):
+        return self._convert_serializer(Request)
+
+    def get_default_response_serializer(self):
+        body_override = self._get_request_body_override()
+        if body_override and body_override is not no_body:
+            return body_override
+
+        return self._convert_serializer(Response)
+
+    def _convert_serializer(self, new_class):
+        serializer = super().get_view_serializer()
+
+        if not serializer:
+            return serializer
+
+        class CustomSerializer(new_class, serializer.__class__):
+            class Meta(getattr(serializer.__class__, 'Meta', BlankMeta)):
+                ref_name = new_class.__name__ + serializer.__class__.__name__
+
+            def __init__(self, *args, **kwargs) -> None:
+                super().__init__(*args, **kwargs)
+                for fieldName, field in dict(self.fields).copy().items():
+                    if fieldName not in dict(self.get_fields()):
+                        self.fields.pop(fieldName)
+
+        new_serializer = CustomSerializer(data=serializer.data)
+        return new_serializer
---
+++ loomi_hub/configuration/openapi.py
@@ -0,0 +1,20 @@
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.urls import path
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="loomi Hub API",
+        default_version='v1',
+        description="Welcome to loomi Hub API Docs",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+
+)
+
+urls_openapi = [
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
