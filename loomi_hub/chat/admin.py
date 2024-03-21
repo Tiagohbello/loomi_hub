bef06dfcdff93b120b685ff1261d5e06469f748d
Tiago Bello <61912931+Tiagohbello@users.noreply.github.com>
2024-03-21 11:49:55 -0300

    Apps models structure (#2)
    * chat app
    
    * post app
    
    * settings addjustments

---
+++ loomi_hub/chat/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from loomi_hub.chat.models import Conversation, Message, ConversationParticipant
+
+
+class ConversationParticipantsInline(admin.TabularInline):
+    model = ConversationParticipant
+    extra = 0
+
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "is_group",
+    )
+    inlines = [ConversationParticipantsInline]
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("conversation", "user", "content")
