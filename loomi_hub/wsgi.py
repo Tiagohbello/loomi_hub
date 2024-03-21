de751ac5d3d0ee048e7162db45482ffce32fd1a0
Tiagohbello <tiagohbello@gmail.com>
2024-03-21 00:10:22 -0300

    first commit

---
+++ loomi_hub/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for loomi_hub project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loomi_hub.settings')
+
+application = get_wsgi_application()
