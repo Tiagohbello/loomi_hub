552a143bfe62f78e7ab3415fb11bd08996086262
Tiagohbello <tiagohbello@gmail.com>
2024-03-24 19:50:05 -0300

    vercel changes

--- loomi_hub/settings.py
+++ loomi_hub/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 
@@ -221,3 +221,5 @@ CHANNEL_LAYERS = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ["*"]
+
+
