f9133e3dcf1cf2b9c49bdace087a10c4773fa810
Tiago Bello <61912931+Tiagohbello@users.noreply.github.com>
2024-03-25 20:02:20 -0300

    filters fix (#12)

--- loomi_hub/chat/apis/filters.py
+++ loomi_hub/chat/apis/filters.py
@@ -5,7 +5,7 @@ from loomi_hub.chat.models import Conversation, Message, ConversationParticipant
 
 
 class ConversationFilter(django_filters.FilterSet):
-    user = django_filters.CharFilter(method="filter_by_user", required=True)
+    user = django_filters.CharFilter(method="filter_by_user", required=False)
 
     class Meta:
         model = Conversation
@@ -21,7 +21,7 @@ class ConversationFilter(django_filters.FilterSet):
 
 class MessageFilter(django_filters.FilterSet):
     conversation = django_filters.UUIDFilter(
-        method="filter_by_conversation", required=True
+        method="filter_by_conversation", required=False
     )
 
     class Meta:
