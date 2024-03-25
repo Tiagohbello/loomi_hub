aab285dc2b0e69de541bce7c6d5c350613bdba77
Tiagohbello <tiagohbello@gmail.com>
2024-03-25 12:31:52 -0300

    deploy settings

--- loomi_hub/asgi.py
+++ loomi_hub/asgi.py
@@ -10,11 +10,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 import django
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "loomi_hub.settings")
 django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 from django.core.asgi import get_asgi_application
 
 from loomi_hub.chat.routing import chat_websocket_urlpatterns
@@ -24,10 +25,11 @@ from loomi_hub.post.routing import post_websocket_urlpatterns
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AllowedHostsOriginValidator(
+        "websocket": OriginValidator(
             TokenAuthMiddleware(
                 URLRouter(chat_websocket_urlpatterns + post_websocket_urlpatterns)
             ),
+            ["*"]
         ),
     }
 )
